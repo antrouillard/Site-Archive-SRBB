7097a679ea1191869c6ed7d910c4297d7ecd7543
Ant <antoine.rouillard@outlook.fr>
2023-03-14 20:32:04 +0100

    Déplacement jojo

--- Exemple site jojo/JavaScript_Test.js
+++
@@ -1,8 +0,0 @@
-new fullpage('#fullpage', {  
-  // get one from 
-  // https://alvarotrigo.com/fullPage/pricing/
-  licenseKey: 'YOUR KEY HERE',
-
-  navigation: true,
-  sectionsColor: ['#ff5f45', '#0798ec', '#fc6c7c', '#fec401'],
-})
